10be796a55a849d5ab383a6d2fd12ef78dbc3244
kushi-bombu <spurthy355@gmail.com>
2025-06-25 22:28:38 +0530

    Add files via upload

---
+++ quiz_app/app.py
@@ -0,0 +1,105 @@
+questions = [
+    {
+        "question": "Which country has more sheep than people?",
+        "options": ["Australia", "India", "New Zealand", "Canada"],
+        "answer": "New Zealand"
+    },
+    {
+        "question": "Which part of the body never grows after birth?",
+        "options": ["Nose", "Eyes", "Legs", "Ears"],
+        "answer": "Eyes"
+    },
+    {
+        "question": "Which fruit floats in water because it's 25% air?",
+        "options": ["Banana", "Apple", "Mango", "Pear"],
+        "answer": "Apple"
+    },
+    {
+        "question": "Which is the only letter not used in any U.S. state name?",
+        "options": ["Q", "Z", "X", "J"],
+        "answer": "Q"
+    },
+    {
+        "question": "What color is hippo’s sweat?",
+        "options": ["Red", "White", "Blue", "Green"],
+        "answer": "Red"
+    },
+    {
+        "question": "Which animal can't jump?",
+        "options": ["Elephant", "Dog", "Tiger", "Kangaroo"],
+        "answer": "Elephant"
+    },
+    {
+        "question": "What is the national animal of Scotland?",
+        "options": ["Lion", "Dragon", "Unicorn", "Horse"],
+        "answer": "Unicorn"
+    },
+    {
+        "question": "Which bird can fly backward?",
+        "options": ["Sparrow", "Crow", "Hummingbird", "Owl"],
+        "answer": "Hummingbird"
+    },
+    {
+        "question": "What does a group of crows called?",
+        "options": ["Gang", "Herd", "Murder", "Pack"],
+        "answer": "Murder"
+    },
+    {
+        "question": "Which planet spins the fastest?",
+        "options": ["Earth", "Saturn", "Jupiter", "Mars"],
+        "answer": "Jupiter"
+    },
+    {
+        "question": "Which insect has ears on its knees?",
+        "options": ["Bee", "Cricket", "Ant", "Mosquito"],
+        "answer": "Cricket"
+    },
+    {
+        "question": "What is the fear of long words called?",
+        "options": ["Tinyphobia", "Longworditis", "Hippopotomonstrosesquipedaliophobia", "Vocabphobia"],
+        "answer": "Hippopotomonstrosesquipedaliophobia"
+    },
+    {
+        "question": "How many noses does a slug have?",
+        "options": ["1", "2", "4", "6"],
+        "answer": "4"
+    },
+    {
+        "question": "Which animal’s poop is cube-shaped?",
+        "options": ["Rabbit", "Koala", "Wombat", "Camel"],
+        "answer": "Wombat"
+    },
+    {
+        "question": "Which fish can blink with both eyes separately?",
+        "options": ["Tuna", "Shark", "Flounder", "Chameleon Fish"],
+        "answer": "Flounder"
+    }
+
+    {
+        "question": "Which fruit has its seeds on the outside?",
+        "options": ["Apple", "Strawberry", "Kiwi", "Papaya"],
+        "answer": "Strawberry"
+    },
+    {
+        "question": "How many hearts does an octopus have?",
+        "options": ["1", "2", "3", "4"],
+        "answer": "3"
+    },
+    {
+        "question": "Which day has the most birthdays in the world?",
+        "options": ["January 1", "October 5", "December 25", "August 15"],
+        "answer": "October 5"
+    },
+    {
+        "question": "Which animal can sleep for 3 years?",
+        "options": ["Bear", "Snail", "Tortoise", "Frog"],
+        "answer": "Snail"
+    },
+    {
+        "question": "What is the tiny pocket inside jeans for?",
+        "options": ["Money", "Watch", "Nails", "Style"],
+        "answer": "Watch"
+    }
+]
+
+
